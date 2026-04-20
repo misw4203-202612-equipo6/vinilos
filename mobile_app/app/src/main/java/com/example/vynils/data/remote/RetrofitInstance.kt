b7c6980800fef4d47e27835e84651ef39df17863
HectorMelgarejo <h.melgarejoa@uniandes.edu.co>
2026-04-19 19:52:15 -0500

    Agregar variables globales (#47)
    Co-authored-by: HectorMelgarejo <h.mmelgarejoa@uniandes.edu.co>

--- mobile_app/app/src/main/java/com/example/vynils/data/remote/RetrofitInstance.kt
+++ mobile_app/app/src/main/java/com/example/vynils/data/remote/RetrofitInstance.kt
@@ -1,10 +1,10 @@
 package com.example.vynils.data.remote
-
+import com.example.vynils.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://vinilos-backend-equipo6-db91c0ab96d3.herokuapp.com/"
+    private const val BASE_URL = BuildConfig.BASE_URL
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
